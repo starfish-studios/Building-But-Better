d1cbe4ceafc41a1a9d6de2a829debfe891a93cbf
crispytwig <48872606+crispytwig@users.noreply.github.com>
2024-09-19 16:48:29 -0500

    Reorganize creative tab

--- src/main/java/com/starfish_studios/bbb/registry/BBBCreativeModeTab.java
+++ src/main/java/com/starfish_studios/bbb/registry/BBBCreativeModeTab.java
@@ -28,30 +28,6 @@ public class BBBCreativeModeTab {
         output.accept(SOUL_BRAZIER);
         output.accept(IRON_FENCE);
 
-        output.accept(OAK_LATTICE);
-        output.accept(SPRUCE_LATTICE);
-        output.accept(BIRCH_LATTICE);
-        output.accept(JUNGLE_LATTICE);
-        output.accept(ACACIA_LATTICE);
-        output.accept(DARK_OAK_LATTICE);
-        output.accept(CRIMSON_LATTICE);
-        output.accept(WARPED_LATTICE);
-        output.accept(MANGROVE_LATTICE);
-        output.accept(BAMBOO_LATTICE);
-        output.accept(CHERRY_LATTICE);
-
-        output.accept(OAK_BALUSTRADE);
-        output.accept(SPRUCE_BALUSTRADE);
-        output.accept(BIRCH_BALUSTRADE);
-        output.accept(JUNGLE_BALUSTRADE);
-        output.accept(ACACIA_BALUSTRADE);
-        output.accept(DARK_OAK_BALUSTRADE);
-        output.accept(CRIMSON_BALUSTRADE);
-        output.accept(WARPED_BALUSTRADE);
-        output.accept(MANGROVE_BALUSTRADE);
-        output.accept(BAMBOO_BALUSTRADE);
-        output.accept(CHERRY_BALUSTRADE);
-
         output.accept(STONE_FRAME);
         output.accept(BLACKSTONE_FRAME);
         output.accept(SANDSTONE_FRAME);
@@ -112,126 +88,151 @@ public class BBBCreativeModeTab {
         // output.accept(BIG_DOOR);
 
 
+        // Oak, Spruce, Birch, Jungle, Acacia, Dark Oak, Crimson, Warped, Mangrove, Bamboo, Cherry
+
         output.accept(OAK_TRIM);
         output.accept(OAK_BEAM);
         output.accept(OAK_BEAM_STAIRS);
         output.accept(OAK_BEAM_SLAB);
-        output.accept(OAK_SUPPORT);
-        output.accept(OAK_FRAME);
         output.accept(OAK_PALLET);
+        output.accept(OAK_SUPPORT);
+        output.accept(OAK_BALUSTRADE);
         output.accept(OAK_WALL);
+        output.accept(OAK_FRAME);
         output.accept(OAK_LANTERN);
         output.accept(OAK_LADDER);
+        output.accept(OAK_LATTICE);
+
 
         output.accept(SPRUCE_TRIM);
         output.accept(SPRUCE_BEAM);
         output.accept(SPRUCE_BEAM_STAIRS);
         output.accept(SPRUCE_BEAM_SLAB);
-        output.accept(SPRUCE_SUPPORT);
-        output.accept(SPRUCE_FRAME);
-        output.accept(SPRUCE_WALL);
-        output.accept(SPRUCE_LANTERN);
         output.accept(SPRUCE_PALLET);
+        output.accept(SPRUCE_SUPPORT);
+        output.accept(SPRUCE_BALUSTRADE);
+        output.accept(SPRUCE_WALL);
+        output.accept(SPRUCE_FRAME);
+        output.accept(SPRUCE_LANTERN);
         output.accept(SPRUCE_LADDER);
+        output.accept(SPRUCE_LATTICE);
 
         output.accept(BIRCH_TRIM);
         output.accept(BIRCH_BEAM);
         output.accept(BIRCH_BEAM_STAIRS);
         output.accept(BIRCH_BEAM_SLAB);
-        output.accept(BIRCH_SUPPORT);
-        output.accept(BIRCH_FRAME);
-        output.accept(BIRCH_WALL);
-        output.accept(BIRCH_LANTERN);
         output.accept(BIRCH_PALLET);
+        output.accept(BIRCH_SUPPORT);
+        output.accept(BIRCH_BALUSTRADE);
+        output.accept(BIRCH_WALL);
+        output.accept(BIRCH_FRAME);
+        output.accept(BIRCH_LANTERN);
         output.accept(BIRCH_LADDER);
+        output.accept(BIRCH_LATTICE);
 
         output.accept(JUNGLE_TRIM);
         output.accept(JUNGLE_BEAM);
         output.accept(JUNGLE_BEAM_STAIRS);
         output.accept(JUNGLE_BEAM_SLAB);
-        output.accept(JUNGLE_SUPPORT);
-        output.accept(JUNGLE_FRAME);
-        output.accept(JUNGLE_WALL);
-        output.accept(JUNGLE_LANTERN);
         output.accept(JUNGLE_PALLET);
+        output.accept(JUNGLE_SUPPORT);
+        output.accept(JUNGLE_BALUSTRADE);
+        output.accept(JUNGLE_WALL);
+        output.accept(JUNGLE_FRAME);
+        output.accept(JUNGLE_LANTERN);
         output.accept(JUNGLE_LADDER);
+        output.accept(JUNGLE_LATTICE);
 
         output.accept(ACACIA_TRIM);
         output.accept(ACACIA_BEAM);
         output.accept(ACACIA_BEAM_STAIRS);
         output.accept(ACACIA_BEAM_SLAB);
-        output.accept(ACACIA_SUPPORT);
-        output.accept(ACACIA_FRAME);
-        output.accept(ACACIA_WALL);
-        output.accept(ACACIA_LANTERN);
         output.accept(ACACIA_PALLET);
+        output.accept(ACACIA_SUPPORT);
+        output.accept(ACACIA_BALUSTRADE);
+        output.accept(ACACIA_WALL);
+        output.accept(ACACIA_FRAME);
+        output.accept(ACACIA_LANTERN);
         output.accept(ACACIA_LADDER);
+        output.accept(ACACIA_LATTICE);
 
         output.accept(DARK_OAK_TRIM);
         output.accept(DARK_OAK_BEAM);
         output.accept(DARK_OAK_BEAM_STAIRS);
         output.accept(DARK_OAK_BEAM_SLAB);
-        output.accept(DARK_OAK_SUPPORT);
-        output.accept(DARK_OAK_FRAME);
-        output.accept(DARK_OAK_WALL);
-        output.accept(DARK_OAK_LANTERN);
         output.accept(DARK_OAK_PALLET);
+        output.accept(DARK_OAK_SUPPORT);
+        output.accept(DARK_OAK_BALUSTRADE);
+        output.accept(DARK_OAK_WALL);
+        output.accept(DARK_OAK_FRAME);
+        output.accept(DARK_OAK_LANTERN);
         output.accept(DARK_OAK_LADDER);
+        output.accept(DARK_OAK_LATTICE);
 
         output.accept(CRIMSON_TRIM);
         output.accept(CRIMSON_BEAM);
         output.accept(CRIMSON_BEAM_STAIRS);
         output.accept(CRIMSON_BEAM_SLAB);
-        output.accept(CRIMSON_SUPPORT);
-        output.accept(CRIMSON_FRAME);
-        output.accept(CRIMSON_WALL);
-        output.accept(CRIMSON_LANTERN);
         output.accept(CRIMSON_PALLET);
+        output.accept(CRIMSON_SUPPORT);
+        output.accept(CRIMSON_BALUSTRADE);
+        output.accept(CRIMSON_WALL);
+        output.accept(CRIMSON_FRAME);
+        output.accept(CRIMSON_LANTERN);
         output.accept(CRIMSON_LADDER);
+        output.accept(CRIMSON_LATTICE);
 
         output.accept(WARPED_TRIM);
         output.accept(WARPED_BEAM);
         output.accept(WARPED_BEAM_STAIRS);
         output.accept(WARPED_BEAM_SLAB);
-        output.accept(WARPED_SUPPORT);
-        output.accept(WARPED_FRAME);
-        output.accept(WARPED_WALL);
-        output.accept(WARPED_LANTERN);
         output.accept(WARPED_PALLET);
+        output.accept(WARPED_SUPPORT);
+        output.accept(WARPED_BALUSTRADE);
+        output.accept(WARPED_WALL);
+        output.accept(WARPED_FRAME);
+        output.accept(WARPED_LANTERN);
         output.accept(WARPED_LADDER);
+        output.accept(WARPED_LATTICE);
 
         output.accept(MANGROVE_TRIM);
         output.accept(MANGROVE_BEAM);
         output.accept(MANGROVE_BEAM_STAIRS);
         output.accept(MANGROVE_BEAM_SLAB);
-        output.accept(MANGROVE_SUPPORT);
-        output.accept(MANGROVE_FRAME);
-        output.accept(MANGROVE_WALL);
-        output.accept(MANGROVE_LANTERN);
         output.accept(MANGROVE_PALLET);
+        output.accept(MANGROVE_SUPPORT);
+        output.accept(MANGROVE_BALUSTRADE);
+        output.accept(MANGROVE_WALL);
+        output.accept(MANGROVE_FRAME);
+        output.accept(MANGROVE_LANTERN);
         output.accept(MANGROVE_LADDER);
+        output.accept(MANGROVE_LATTICE);
 
         output.accept(BAMBOO_TRIM);
         output.accept(BAMBOO_BEAM);
         output.accept(BAMBOO_BEAM_STAIRS);
         output.accept(BAMBOO_BEAM_SLAB);
-        output.accept(BAMBOO_SUPPORT);
-        output.accept(BAMBOO_FRAME);
-        output.accept(BAMBOO_WALL);
-        output.accept(BAMBOO_LANTERN);
         output.accept(BAMBOO_PALLET);
+        output.accept(BAMBOO_SUPPORT);
+        output.accept(BAMBOO_BALUSTRADE);
+        output.accept(BAMBOO_WALL);
+        output.accept(BAMBOO_FRAME);
+        output.accept(BAMBOO_LANTERN);
         output.accept(BAMBOO_LADDER);
+        output.accept(BAMBOO_LATTICE);
 
         output.accept(CHERRY_TRIM);
         output.accept(CHERRY_BEAM);
         output.accept(CHERRY_BEAM_STAIRS);
         output.accept(CHERRY_BEAM_SLAB);
-        output.accept(CHERRY_SUPPORT);
-        output.accept(CHERRY_FRAME);
-        output.accept(CHERRY_WALL);
-        output.accept(CHERRY_LANTERN);
         output.accept(CHERRY_PALLET);
+        output.accept(CHERRY_SUPPORT);
+        output.accept(CHERRY_BALUSTRADE);
+        output.accept(CHERRY_WALL);
+        output.accept(CHERRY_FRAME);
+        output.accept(CHERRY_LANTERN);
         output.accept(CHERRY_LADDER);
+        output.accept(CHERRY_LATTICE);
 
 
         // region LAYERS
